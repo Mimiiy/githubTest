b1cb5178bba230efe1485bdf18e96fc82e61a511
Maryam <maryamabdulbaki@yahoo.com>
2025-02-19 14:24:18 +0100

    seperating unit and manual testing

--- API/test_api.py
+++ API/test_api.py
@@ -1,14 +1,33 @@
 import pytest
 import requests
+import unittest.mock as mock
 
-#call the base url
+#manual testing
 def test_git_api ():
     query = "Python"
     base_url = f"https://api.github.com/search/users?q={query}"
-
     response = requests.get(base_url)
+    
     if response.status_code == 200:
         formatted_text = response.json()
         print(f"{formatted_text}")
     else:
         print(f"unsuccessful query")
+
+def get_users():
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise requests.HTTPError
+
+@mock.patch("requests.get")    
+def test_get_users(mock_get):
+    mock_response = mock.Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {"id": 1, "name": "Maryam"}
+    
+    mock_get.return_value = mock_response
+    data = get_users()
+    assert data == {"id": 1, "name": "Maryam"}
+
