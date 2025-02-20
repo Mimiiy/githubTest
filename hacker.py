7a72dd20874871e8233a24931cf28380ff4c4b53
Maryam <maryamabdulbaki@yahoo.com>
2025-02-20 16:38:29 +0100

    hacker rank challenges

---
+++ hacker.py
@@ -0,0 +1,13 @@
+
+#challenge 1: Print the list of integers from  through  as a string, without spaces.
+def main():
+    x = int(input())
+    y = return_sequence(x)
+
+def return_sequence(n):
+    for i in range(1,n+1):
+        print(f"{i}", end="")
+
+if __name__ == "__main__":
+    main()
+
