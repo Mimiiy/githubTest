8f54dfed8f768345f2b667a97f947b197e7844f2
Maryam <maryamabdulbaki@yahoo.com>
2025-03-03 04:51:50 +0100

    modifying get api

--- API/test_api.py
+++ API/test_api.py
@@ -1,42 +1,36 @@
 import pytest
 import requests
 import unittest.mock as mock
+import api
 
-#manual testing
-def test_git_api ():
-    query = "Python"
-    base_url = f"https://api.github.com/search/users?q={query}"
-    response = requests.get(base_url)
-    
-    if response.status_code == 200:
-        formatted_text = response.json()
-        print(f"{formatted_text}")
-    else:
-        print(f"unsuccessful query")
 
-def get_users():
-    response = requests.get("https://jsonplaceholder.typicode.com/users")
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise requests.HTTPError
+#positive repsonse code
+@pytest.mark.parametrize("url, response_code", [("https://api.github.com/search/users?q={Python}", 200), 
+                                 ("https://fakerestapi.azurewebsites.net/api/v1/Activities",200),
+                                 ("https://jsonplaceholder.typicode.com/users", 200)]
+                         )
+def test_get_api(url, response_code):
+    response = requests.get(url)
+    assert response.status_code == response_code
+
 
 #unit testing
+#This section tests for a successful API call
 @mock.patch("requests.get")    
 def test_get_users_successful(mock_get):
     mock_response = mock.Mock()
     mock_response.status_code = 200
     mock_response.json.return_value = {"id": 1, "name": "Maryam"}
     mock_get.return_value = mock_response
-    data = get_users()
+    data = api.get_users()
     assert data == {"id": 1, "name": "Maryam"}
 
-#This section is testing the for an unsuccessful API call
+#This section tests for an unsuccessful API call
 @mock.patch("requests.get")
 def test_get_users_error(mock_get):
     mock_response = mock.Mock()
     mock_response.status_code = 000
     mock_get.return_value = mock_response
     with pytest.raises(requests.HTTPError):
-        get_users()
+        api.get_users()
 
